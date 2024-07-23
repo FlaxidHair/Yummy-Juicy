df22c22071db9a47dcc58ea40b1a35491b0440b3
Flaxid Hair <kmarlou@icloud.com>
2024-07-23 12:57:36 +0300

    add: add template constructor for recipe

--- src/pages/Recipe.vue
+++ src/pages/Recipe.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="main">
-        recipe
+        <h2 class="recipe-title title">
+            Рецепты
+        </h2>
+        <input type="text" v-model="$store.state.recipe.nameRecipe">
+        <input type="text" v-model="$store.state.recipe.nameIng">
+        <button @click="$store.commit('templateRecipe')">Добавить рецепт</button>
+        <div v-for="recipe in $store.state.recipe.recipies2" :key="recipe">
+            {{ recipe }}
+        </div>
+        
     </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -4,6 +4,14 @@ const store = createStore({
   state() {
     return {
       show: false,
+      recipe: {
+        name: [],
+        ing: [],
+        nameIng: "",
+        nameRecipe: "",
+        recipies: [],
+        recipies2: [],
+      },
     };
   },
   mutations: {
@@ -13,6 +21,20 @@ const store = createStore({
     },
     closeModal(state) {
       state.show = false;
+    },
+    addNameRecipe(state) {
+      state.recipe.name.push(state.recipe.nameRecipe);
+      state.recipe.ing.push(state.recipe.nameIng);
+    },
+    templateRecipe(state) {
+      this.commit("addNameRecipe");
+      state.recipe.recipies.push(state.recipe.name.at(-1));
+      state.recipe.recipies.push(state.recipe.ing.at(-1));
+      const obj = {
+        name: state.recipe.name.at(-1),
+        ing: state.recipe.ing.at(-1),
+      };
+      state.recipe.recipies2.push(obj);
     },
   },
 });
