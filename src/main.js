8255d0237e3de0e0540cadfd134e2718323049a4
Flaxid Hair <kmarlou@icloud.com>
2024-07-23 09:06:18 +0300

    refactor: install vuex library

--- src/main.js
+++ src/main.js
@@ -3,7 +3,21 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createStore } from "vuex";
 const app = createApp(App);
 
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
+app.use(store);
 app.use(router);
 app.mount("#app");
