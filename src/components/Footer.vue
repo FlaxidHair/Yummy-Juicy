4a81309a6ba54a9803cf93f9c750e52eb0978c78
FlaxidHair <mrkmarlou@gmail.com>
2024-07-24 21:01:03 +0300

    feat: add elements in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-    <footer>Footer</footer>
+    <footer class="footer">
+        <div class="footer-logo">
+            <img src="../assets/images/logo.png" alt="Logo">
+            <h2 class="footer-logo-text">RecipeBook</h2>
+        </div>
+        <p>© Recipe Book  </p>
+        <span>Minsk 2024</span>
+    </footer>
 </template>
 
 <script>
