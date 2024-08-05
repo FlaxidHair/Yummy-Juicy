08e4c9255e99c41c48d526ece109d00194ea0812
Flaxid Hair <kmarlou@icloud.com>
2024-08-05 15:13:57 +0300

    refactor: change sze of image in random DIsh

--- src/components/RandomDish.vue
+++ src/components/RandomDish.vue
@@ -6,17 +6,17 @@
                 <v-card height="400" :elevation="4" class="w-33 d-flex flex-column align-center">
                     <v-card-subtitle class="mt-2">Мясное</v-card-subtitle>
                     <v-card-title>Куриное филе в пармезане</v-card-title>
-                    <img src="" alt="Image of dish">
+                    <img src="" alt="Image of dish" class="w-100 h-100">
                 </v-card>
                 <v-card height="400" :elevation="4" class="w-33 d-flex flex-column align-center">
                     <v-card-subtitle class="mt-2">Гарнир</v-card-subtitle>
                     <v-card-title>Макароны</v-card-title>
-                    <img src="" alt="Image of dish">
+                    <img src="" alt="Image of dish" class="w-100 h-100">
                 </v-card>
                 <v-card height="400" :elevation="4" class="w-33 d-flex flex-column align-center">
                     <v-card-subtitle class="mt-2">Салат</v-card-subtitle>
                     <v-card-title>Салат с огурцов и помидор</v-card-title>
-                    <img src="" alt="Image of dish">
+                    <img src="" class="w-100 h-100" alt="Image of dish">
                 </v-card>
             </div>
         </v-container>
