66b33891cb43ba810b20e5c2964a3d1ac7aee7d7
Flaxid Hair <kmarlou@icloud.com>
2024-08-05 11:38:30 +0300

    feat: add header element mark

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,60 @@
 <template>
-
  <div class="main d-flex justify-center">
-  <v-app-bar :elevation="2" rounded class="position-relative w-75 d-flex justify-center">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  <div>
+  <v-app-bar :elevation="3" rounded class="position-relative w-75 d-flex justify-center">
+  <div class="ml-8">
     <v-app-bar-title class="d-flex ga-3 text-no-wrap">
         <RouterLink to="/">
             Yummy&Juicy
-            <v-icon color="blue" class="ma-0" icon="mdi-chef-hat"></v-icon>
         </RouterLink>
 
-    </v-app-bar-title>
+    </v-app-bar-title>    
 </div>
-    <v-btn class="d-flex justify-center"><v-icon icon="mdi-magnify"></v-icon></v-btn>
+<div class="ml-auto" >
+    <ul class="d-flex">
+        <RouterLink to="/dishes"><li><v-btn>Блюда</v-btn></li></RouterLink>
+        <RouterLink to="/recipies"><li><v-btn>Рецепты</v-btn></li></RouterLink>
+        <RouterLink to="/template-recipe"><li><v-btn>Создать рецепт</v-btn></li></RouterLink>
+        <RouterLink to="notes"><li><v-btn>Заметки</v-btn></li></RouterLink>
+    </ul>
+</div>
+<div class="ml-auto " >
+    <v-card-text >
+      <v-text-field
+        width="20vw"
+        :loading="loading"
+        append-inner-icon="mdi-magnify"
+        density="compact"
+        label="Найти рецепт"
+        variant="solo"
+        hide-details
+        single-line
+        @click:append-inner="onClick"
+      ></v-text-field>
+    </v-card-text>
+</div>
 </v-app-bar>
 </div>
 
 </template>
 
-<script setup>
+<script>
+ export default {
+    data: () => ({
+      loaded: false,
+      loading: false,
+    }),
 
+    methods: {
+      onClick() {
+        this.loading = true
+
+        setTimeout(() => {
+          this.loading = false
+          this.loaded = true
+        }, 1500)
+      },
+    },
+  }
 </script>
 
 <style >
