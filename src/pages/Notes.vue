c7861e11763b9b2b05738502726d3c3026d8dcb3
Flaxid Hair <kmarlou@icloud.com>
2024-08-15 11:15:45 +0300

    feat: release Addition Notes

--- src/pages/Notes.vue
+++ src/pages/Notes.vue
@@ -2,29 +2,42 @@
     <div class="main d-flex flex-column align-center">
         <h2 class="mt-10 mb-5">Добавить заметку</h2>
     <v-container class="">
+
         <div class="w-50 mr-auto ml-auto border-thin mb-5">
+            <form ref="forma">
             <div class="d-flex">
-        <v-text-field  label="Введите заголовок" tile variant="solo"  hide-details class=""></v-text-field>
-                <v-btn class="elevation-2" width="120px" tile height="56px" flat>Добавить</v-btn>
-    </div>
-        <v-textarea label="Введите заметку" no-resize max-rows="10" tile variant="filled" hide-details class=""></v-textarea>
+        <v-text-field name="Title"  label="Введите заголовок" tile variant="solo"  hide-details class=""></v-text-field>
+                <v-btn @click="postNote()" class="elevation-2" width="120px" tile height="56px" flat>Добавить</v-btn>
         </div>
+        <v-textarea name="Subtitle" label="Введите заметку" no-resize max-rows="10" tile variant="filled" hide-details class=""></v-textarea>
+    </form>    
+    </div>
+
         <v-divider></v-divider>
     </v-container>
-    <v-container class="d-flex" v-for="note in store.notes" :key="note">
-        <v-card class="d-flex flex-column pa-5">
-            <v-card-title>{{ note.Title }}</v-card-title>
-        </v-card>
-    </v-container>
+    <div class="d-flex ga-5 flex-wrap">
+   <v-card class="" v-for="note in store.getterNotes.notes" :key="note">
+        <v-card-title >{{ note.Title }}</v-card-title>
+        <v-card-text >{{ note.Subtitle }}</v-card-text>
+   </v-card>
 </div>
-
+</div>
 </template>
 
-<script>
+<script setup>
 import {useStoreRecipies} from '../stores/storeRecipies'
+import {ref} from 'vue'
 const store = useStoreRecipies();
+const forma = ref(null)
+function postNote(){
+          fetch('https://script.google.com/macros/s/AKfycbwn9VGFkrEJWUpIR73XgknPNKpRsRlTdA4ZsRJ32SPsBYsPQ86mv564us55E6lgedoJ6Q/exec',{
+            method:"POST",
+            body:new FormData(forma.value),
+          }).then((response)=>{
+            if(response.status==200) {
+              store.getNotes()    
+              alert("Заметка добавлена")
+            }
+          })   
+        }
 </script>
-
-<style>
-
-</style>
