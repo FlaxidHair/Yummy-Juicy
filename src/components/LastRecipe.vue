cf65d1df684120c596d479eafbe05d9e6044adbd
Flaxid Hair <kmarlou@icloud.com>
2024-08-27 12:10:24 +0300

    refactor: change size in last recipe

--- src/components/LastRecipe.vue
+++ src/components/LastRecipe.vue
@@ -5,13 +5,13 @@
             <div>
                 <v-dialog max-width="800">
                     <template v-slot:activator="{ props: activatorProps }">
-                        <div class="d-flex ga-5">
+                        <div class="d-flex ga-5 w-7 justify-center">
                             <div v-for="recipe in store.getterLastRecipies" ref="recipe" :key="recipe"
-                                class="d-flex w-25 ga-5 mb-7">
+                                class="d-flex mb-7">
                                 <v-hover v-slot="{ isHovering, props }" open-delay="50">
                                     <div @click="() => { store.selectRecipe = recipe }" :item="recipe"
                                         class="w-100 cursor-pointer d-flex" v-bind="activatorProps">
-                                        <v-card class="d-flex flex-column align-center" height="600"
+                                        <v-card class="d-flex flex-column align-center" width="400px" height="500"
                                             :class="isHovering, props" v-bind="props" :elevation="isHovering ? 8 : 2">
                                             <v-card-title class="mr-auto ml-auto">{{ recipe.Name }}</v-card-title>
                                             <div class="d-flex w-100 justify-center">
@@ -23,9 +23,9 @@
                                                 <div class="border-thin mr-1 pa-1 align-self-center">
                                                     {{ recipe.Time }}</div>
                                             </div>
-                                            <v-card-text class="h-25 text-caption">{{ recipe.Description
+                                            <v-card-text class="h-25 text-caption border-thin overflow-hidden ma-2">{{ recipe.Description
                                                 }}</v-card-text>
-                                            <img :src="recipe.Image" class="w-100 h-50"
+                                            <img :src="recipe.Image" width="500px" class="h-50"
                                                 alt="Тут могло быть изображения еды">
                                         </v-card>
                                     </div>
