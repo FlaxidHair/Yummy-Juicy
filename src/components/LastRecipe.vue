57b6fc808dd03ea05016669fde3bfe4c12ecd5ae
Flaxid Hair <kmarlou@icloud.com>
2024-09-04 08:44:45 +0300

    feat: add responsive marks for last recipe block

--- src/components/LastRecipe.vue
+++ src/components/LastRecipe.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-        <v-container class="mt-15 w-100 d-flex bg-white elevation-3 flex-column">
+        <v-container class="mt-15 w-xl-100 w-lg-100 w-md-100 w-sm-75 d-flex bg-white elevation-3 flex-column">
             <h2 class="mb-10 align-self-center">Последние рецепты</h2>
-            <div>
+            <div class="d-flex justify-center">
                 <v-dialog max-width="800">
                     <template v-slot:activator="{ props: activatorProps }">
-                        <div class="d-flex ga-5 w-7 justify-center">
+                        <div class="d-flex ga-xl-5 gc-lg-16 ga-md-0 justify-center  justify-lg-center justify-xl-start justify-sm-center ga-sm-1 flex-xl-nowrap flex-lg-wrap flex-md-wrap flex-sm-wrap flex-wrap ga-3 w-xl-100 mx-xl-auto w-md-100 w-lg-100 w-sm-100">
                             <div v-for="recipe in store.getterLastRecipies" ref="recipe" :key="recipe"
-                                class="d-flex mb-7">
+                                class="d-flex mb-7 w-xl-50 w-lg-33 w-md-50 w-sm-100 w-75  justify-center">
                                 <v-hover v-slot="{ isHovering, props }" open-delay="50">
                                     <div @click="() => { store.selectRecipe = recipe }" :item="recipe"
-                                        class="w-100 cursor-pointer d-flex" v-bind="activatorProps">
-                                        <v-card class="d-flex flex-column  align-center" width="400px" height="500"
+                                        class="w-xl-100 w-lg-100 w-sm-100 w-75 cursor-pointer justify-center d-flex" v-bind="activatorProps">
+                                        <v-card class="d-flex flex-column align-center" width="400px" height="500"
                                             :class="isHovering, props" v-bind="props" :elevation="isHovering ? 8 : 2">
-                                            <v-card-title class="mr-auto ml-auto">{{ recipe.Name }}</v-card-title>
+                                            <v-card-title class="mr-auto ml-auto text-wrap text-center">{{ recipe.Name }}</v-card-title>
                                             <div class="d-flex w-100 justify-center">
                                                 <div class="border-thin ml-1 pa-1 align-self-center">{{ recipe.Category
                                                     }}</div>
