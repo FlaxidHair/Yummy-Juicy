bb8fbc2c34786d2e0e2eb6491a9d247a53232f8f
Flaxid Hair <kmarlou@icloud.com>
2024-09-04 15:16:09 +0300

    feat: end responsive marks of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="mt-10 d-flex">
-    <v-footer class="bg-red-accent-1" height="200px">
-      <v-row justify="center" no-gutters>
-        <ul class="d-flex ga-5 flex-wrap">
+    <v-footer class="bg-red-accent-1 d-flex flex-column justify-center w-100">
+        <ul class="d-flex justify-center ga-xl-5 ga-lg-5 ga-md-5 ga-sm-2 ga-2 mt-xl-16 mt-lg-16 mt-md-16 mt-sm- mt-2 mb-xl-16 mb-lg-16 mb-md-16 mb-sm-10 mb-5 flex-wrap w-75">
           <RouterLink to="/">
-            <li><v-btn class="bg-transparent elevation-1 text-white">Главная</v-btn></li>
+            <li><v-btn class="bg-transparent elevation-0 text-white">Главная</v-btn></li>
           </RouterLink>
           <RouterLink to="/favorite">
-            <li><v-btn class="bg-transparent elevation-1 text-white">Избранное</v-btn></li>
+            <li><v-btn class="bg-transparent elevation-0 text-white">Избранное</v-btn></li>
           </RouterLink>
           <RouterLink to="/recipies">
-            <li><v-btn class="bg-transparent elevation-1 text-white">Рецепты</v-btn></li>
+            <li><v-btn class="bg-transparent elevation-0 text-white">Рецепты</v-btn></li>
           </RouterLink>
           <RouterLink to="/template-recipe">
-            <li><v-btn class="bg-transparent elevation-1 text-white">Создать рецепт</v-btn></li>
+            <li><v-btn class="bg-transparent elevation-0 text-white">Создать рецепт</v-btn></li>
           </RouterLink>
           <RouterLink to="notes">
-            <li><v-btn class="bg-transparent elevation-1 text-white">Заметки</v-btn></li>
+            <li><v-btn class="bg-transparent elevation-0 text-white">Заметки</v-btn></li>
           </RouterLink>
         </ul>
-        <v-col class="text-center mt-15" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Yummy&Juice</strong>
-        </v-col>
-      </v-row>
+        <div>
+          <span class="text-white">{{ new Date().getFullYear() }} —</span>  <strong class="text-white">Yummy&Juice</strong>
+        </div>
     </v-footer>
   </div>
 </template>
